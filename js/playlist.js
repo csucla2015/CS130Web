a1ac0e4b5129be9c2180226813bde547e717c439
Andy Yoon <ayoon212@gmail.com>
2014-11-11 23:09:49 -0800

    restructure directory

---
+++ js/playlist.js
@@ -0,0 +1,14 @@
+$(".create-form").on("submit", function(e) {
+  $.post(
+    "https://72bc04a9.ngrok.com/playlist",
+    $(".create-form").serialize()
+    function(res) {
+      $(".response").html(res);
+    }
+  );
+  return false;
+});
+
+function create(res) {
+  alert('helo');
+}
