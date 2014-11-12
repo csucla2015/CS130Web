1327d26d37bca1a4ed4a1e385f9c9a459cfb09c6
Calvin <calvin.chan.h@gmail.com>
2014-11-11 23:24:07 -0800

    API for GET playlist done

--- api/controllers/PlaylistController.js
+++ api/controllers/PlaylistController.js
@@ -6,6 +6,27 @@
  */
 
 module.exports = {
+    index : function(req, res) {
+        var phoneNumber = req.param('phonenumber');
+        if (phoneNumber) {
+            // if phoneNumber exists then return one playlist
+            Playlist.findOne({
+                'phoneNumber' : phoneNumber
+            }, function(err, playlist) {
+                if (err) {
+                    return res.serverError(err);
+                }
+                return res.json(playlist);
+            });
+        } else {
+            Playlist.find({}, function(err, playlists) {
+                if (err) {
+                    return res.serverError(err);
+                }
+                return res.json(playlists);
+            });
+        }
+    },
     create : function(req,res) {
         var playlistName = req.param('name');
         Twilio.getFreeNumbers(function(err, numbers) {
@@ -20,8 +41,10 @@ module.exports = {
                     playlistName : playlistName,
                     phoneNumber : number
                 }, function(err, playlist) {
-                    console.log("HELLO WORLD");
-                    res.json(playlist);
+                    if (err) {
+                        return res.serverError(err);
+                    }
+                    return res.json(playlist);
                 });
             }
         });
